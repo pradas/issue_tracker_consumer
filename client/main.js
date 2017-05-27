2dbba5787648969b5ffba05667eba5037bee6e54
Adrian Borrego Martinez <adrian.borrego@est.fib.upc.edu>
2017-05-27 16:19:55 +0000

    Cambio de pagina hecho

--- client/main.js
+++ client/main.js
@@ -2,13 +2,25 @@ var mainApp = angular.module("mainApp", ['ngRoute']);
 
 mainApp.config(function($routeProvider) {
 	$routeProvider
-		.when('/index', {
-			templateUrl : 'index.html',
+		.when('/home', {
+			templateUrl: 'home.html',
+			controller: 'StudentController'
 		})
 		.when('/create', {
-			templateUrl : 'create.html',
+			templateUrl: 'create.html',
+			controller: 'StudentController'
 		})
 		.otherwise({
-			redirectTo: '/index'
+			redirectTo: '/home'
 		});
+});
+
+mainApp.controller('StudentController', function($scope) {
+	$scope.students = [
+		{name: 'Mark Waugh', city:'New York'},
+		{name: 'Steve Jonathan', city:'London'},
+		{name: 'John Marcus', city:'Paris'}
+	];
+
+	$scope.message = "Click on the hyper link to view the students list.";
 });
