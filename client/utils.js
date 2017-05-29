df620c6cd0a0dff1b1ff0fcc2465bc86f66c5f1f
Joan Pages <joan.pages.esbri@gmail.com>
2017-05-29 18:12:38 +0000

    utils

---
+++ client/utils.js
@@ -0,0 +1,47 @@
+ function timeDifference(time) {
+ 		var previous = new Date(time);
+    var current = new Date();
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+    
+    var elapsed = current - previous;
+    
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+    
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+    
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+         return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+    
+    else if (elapsed < msPerYear) {
+         return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+    
+    else {
+         return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
+
+function changeTime(){
+    var times = document.getElementsByTagName("TIME");
+    
+    console.log(times.length);
+    for (var t of times){
+        console.log(t.innerHTML);
+        t.innerHTML = timeDifference(t.innerHTML);
+    }
+}
+
+changeTime();
