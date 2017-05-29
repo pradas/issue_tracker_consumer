21215f05f94c166c9f50563ecdc1c54df348cdaf
Gerard Pradas <gerard.pradas.san.jose@est.fib.upc.edu>
2017-05-29 17:52:20 +0000

    fixed post issue

--- client/getIssuesCtrl.js
+++ client/getIssuesCtrl.js
@@ -3,23 +3,23 @@
 var app = angular.module('mainApp');
 
 app.controller('getIssuesCtrl', function($scope, $http) {
-    var config = {headers:  {
-            'Authorization': '110490905416396817633'
-        }
-    };
-    $http.get("http://fast-lake-76623.herokuapp.com/api/issues", config).then(function(response) {
+    $http.get("http://fast-lake-76623.herokuapp.com/api/issues").then(function(response) {
         $scope.myData = response.data.issues;
     });
 });
 
 app.controller('createIssue', function($scope, $http) {
     
+    $http.get("http://fast-lake-76623.herokuapp.com/api/users").then(function(response) {
+        $scope.users = response.data.users;
+    });
+    
     $scope.submit = function () {
        // use $.param jQuery function to serialize data from JSON 
         var data = {
             title: $scope.title,
             description: $scope.description,
-            user_id: $scope.user,
+            user_id: $scope.userissue,
             kind: $scope.kind,
             priority: $scope.prior
         };
@@ -33,9 +33,11 @@ app.controller('createIssue', function($scope, $http) {
 
         $http.post("http://fast-lake-76623.herokuapp.com/api/issues", data, config)
         .success(function (data, status, headers, config) {
-            window.location.href = '/';
+            console.log(data)
+;            window.location.href = '/';
         })
         .error(function (data, status, header, config) {
+            console.log(data);
             $scope.ResponseDetails = "Data: " + data +
                 "<hr />status: " + status +
                 "<hr />headers: " + header +
